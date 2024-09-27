3c627797117032bc936bbc88647347180abab36c
chaewon02~ <glgl246@naver.com>
2024-09-27 22:43:49 +0900

    ci: 포트 허용

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
+    port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080', // 백엔드 서버 주소
+      "/api": {
+        target: "http://localhost:8080", // 백엔드 서버 주소
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
